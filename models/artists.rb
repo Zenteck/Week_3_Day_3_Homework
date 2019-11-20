928e989656e944ca35701bd2bc661e1b89a3c958
Alan <spambox+github@zenteck.co.uk>
2019-11-20 15:56:48 +0000

    jic

---
+++ models/artists.rb
@@ -0,0 +1,57 @@
+require_relative('../db/sql_runner.rb')
+require_relative('./albums.rb')
+
+class Artist
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(info)
+    @id = info['id'].to_i() if info['id']
+    @name = info['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists name VALUES $1 RETURNING id;"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i()
+  end
+
+  def self.delete_all()
+    SqlRunner.run("DELETE FROM artists;")
+  end
+
+  #edit
+  def edit()
+    sql = "UPDATE artists SET name = $1 WHERE id $2;"
+    values = [@name]
+    resut = SqlRunner.run(sql, values)
+    return result
+  end
+
+  #delete
+  def delete()
+    sql = "DELETE FROM artists WHERE id = $1;"
+    values = [@id]
+    result = SqlRunner.run(sql, values)
+    return result
+  end
+  #find
+  def find_albums()
+    sql = "SELECT * FROM albums WHERE artist_id = $1;"
+    values = [@id]
+    albums = SqlRunner.run(sql, values)
+    return albums.map{|album| Album.new(album)}
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM artists WHERE id = $1;"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    return nil if result.count == 0
+    artist_hash = result[0]
+    return Artist.new(artist_hash)
+  end
+
+end
